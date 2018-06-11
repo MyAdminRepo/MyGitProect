6b88cec1d557ca866c90a4be67b1e2f1f710f9bb
rahulTL1 <rahul@gmail.com>
2018-06-11 13:36:17 +0530

    dev1 write condition in if statement

--- SimplePattern/src/com/rp/test/demoTest.java
+++ SimplePattern/src/com/rp/test/demoTest.java
@@ -5,6 +5,8 @@ public class demoTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		boolean flag=true;
+		
 		for (int i = 1; i <=5; i++) {
 			for (int j = 1; j <5; j++) {
 				
@@ -24,6 +26,12 @@ public class demoTest {
 			System.out.println();
 			 
 		}
+		
+		if (flag) {
+			
+		}
+		
+		
 
 	}
 
